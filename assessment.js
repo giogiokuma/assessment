b40f3822159cf767fc299a0c015284678f16782b
Misako Ohata <misa@ohatadesign.com>
2017-09-16 08:22:49 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -1,16 +1,28 @@
+/**
+* 名前の文字列を渡すと診断機能を渡す関数
+* @param {string} userName ユーザーの名前
+* @return {string} 診断結果
+*
+**/
+
+
+
+
+
+
 (function () {
     'use strict';
-    const userNameInput = document.getElementById('user-name');
-    const assessmentButton = document.getElementById('assessment');
-    const resultDivided = document.getElementById('result-area');
-    const tweetDivided = document.getElementById('tweet-area');
+    const userNameInput = document.getElementById("user-name"); 
+    const assessmentButton = document.getElementById("assessment"); 
+    const resultDivided = document.getElementById("result-area"); 
+    const tweetDivided = document.getElementById("tweet-area");
 
     /**
-    * 指定した要素の子どもを全て除去する
+    * 指定した要素の子どもを全て削除する
     * @param {HTMLElement} element HTMLの要素
     */
     function removeAllChildren(element) {
-        while (element.firstChild) { // 子どもの要素があるかぎり除去
+        while (element.firstChild) { // 子どもの要素があるかぎり削除
             element.removeChild(element.firstChild);
         }
     }
@@ -32,8 +44,7 @@
         paragraph.innerText = result;
         resultDivided.appendChild(paragraph);
 
-        // ツイートエリアの作成
-        removeAllChildren(tweetDivided);
+
         const anchor = document.createElement('a');
         const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D&text='
             + encodeURIComponent(result);
@@ -43,7 +54,13 @@
         tweetDivided.appendChild(anchor);
 
         twttr.widgets.load();
+
     };
+userNameInput.onkeydown = (event) => {
+            if (event.keyCode === 13) {
+                assessmentButton.onclick();
+            };
+        }
 
     const answers = [
         '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
@@ -63,34 +80,38 @@
         '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
         '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
     ];
+/**
+* 名前を渡すと、診断結果を返す関数
+* @param {string} userName
+* @return {string} 診断結果
+*/
+function assessment(userName) {
+  // TODO 診断処理を実装
 
-    /**
-    * 名前の文字列を渡すと診断結果を返す関数
-    * @param {string} userName ユーザーの名前
-    * @return {string} 診断結果
-    */
-    function assessment(userName) {
-        // 全文字のコード番号を取得してそれを足し合わせる
-        let sumOfcharCode = 0;
-        for (let i = 0; i < userName.length; i++) {
-            sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
-        }
+  let sumOfcharCode = 0;
 
-        // 文字のコード番号の合計を回答の数で割って添字の数値を求める
-        const index = sumOfcharCode % answers.length;
-        let result = answers[index];
+  for (var i = 0; i < userName.length; i++) {
+    sumOfcharCode = sumOfcharCode + userName.charCodeAt(i); 
+  }
 
-        result = result.replace(/{userName}/g, userName);
-        return result;
-    }
+  // 文字コードの合計を回答の数で割ってインデックスを求める
+  const index = sumOfcharCode % answers.length;
+  let result = answers[index];
+  result = result.replace(/\{userName\}/g, userName);
 
-    // テストコード
-    console.assert(
-        assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
-        '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
+  // TODO {userName} をユーザーの名前に置き換える
+  return result;
+
+  // テストコード
+  console.assert(
+    assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+    '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
     );
-    console.assert(
-        assessment('太郎') === assessment('太郎'),
-        '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
+  console.assert(
+    assessment('太郎') === assessment('太郎'),
+    '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
     );
+
+}
+
 })();
